32a84e308b4a7e77c1f3d9281d231444ed55f160
deaththekidd <ohiemidivine7@gmail.com>
2023-09-10 00:44:59 +0100

    update

--- dist/server.js
+++ dist/server.js
@@ -5,9 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const errorhandler_1 = __importDefault(require("errorhandler"));
 const app_1 = __importDefault(require("./app"));
-const https_1 = __importDefault(require("https"));
-const http_1 = __importDefault(require("http"));
-const fs_1 = __importDefault(require("fs"));
 /**
  * Error Handler. Provides full stack
  */
@@ -17,15 +14,15 @@ if (process.env.NODE_ENV === "development") {
 /**
  * Start Express server.
  */
-const httpsOptions = {
-    Key: fs_1.default.readFileSync("./src/certificates/ssl.key", "utf8"),
-    cert: fs_1.default.readFileSync("./src/certificates/certificate.crt", "utf8"),
-};
-const httpServer = http_1.default.createServer(app_1.default).listen(process.env.PORT || 3001);
-const httpsServer = https_1.default.createServer(httpsOptions, app_1.default);
-const server = httpsServer.listen(app_1.default.get("port"), () => {
+// const httpsOptions: any = {
+// 	Key: fs.readFileSync("./src/certificates/ssl.key", "utf8"),
+// 	cert: fs.readFileSync("./src/certificates/certificate.crt", "utf8"),
+// };
+// const httpServer = http.createServer(app).listen(process.env.PORT || 3001);
+// const httpsServer = https.createServer(httpsOptions, app);
+const server = app_1.default.listen(app_1.default.get("port"), () => {
     console.log("  App is running at http://localhost:%d in %s mode", app_1.default.get("port"), app_1.default.get("env"));
     console.log("  Press CTRL-C to stop\n");
 });
-exports.default = httpServer;
+exports.default = server;
 //# sourceMappingURL=server.js.map
