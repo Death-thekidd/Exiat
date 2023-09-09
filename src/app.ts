8f3a409b5d1cd12bc298de81e29bec08012bbed7
deaththekidd <ohiemidivine7@gmail.com>
2023-09-10 00:55:35 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -77,7 +77,7 @@ sequelize
 // 	});
 
 // Express configuration
-app.set("port", process.env.PORT || 8443);
+app.set("port", process.env.PORT || 8080);
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 app.use(compression());
