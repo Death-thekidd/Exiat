d9585d31b4b33491dc44fdd35fd81ca7e213dfc8
deaththekidd <ohiemidivine7@gmail.com>
2023-09-10 04:56:35 +0100

    uptatata

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import errorHandler from "errorhandler";
 import app from "./app";
+import https from "https";
+import http from "http";
+import fs from "fs";
 
 /**
  * Error Handler. Provides full stack
@@ -8,4 +11,23 @@ if (process.env.NODE_ENV === "development") {
 	app.use(errorHandler());
 }
 
-export default app;
+/**
+ * Start Express server.
+ */
+// const httpsOptions: any = {
+// 	Key: fs.readFileSync("./src/certificates/ssl.key", "utf8"),
+// 	cert: fs.readFileSync("./src/certificates/certificate.crt", "utf8"),
+// };
+
+// const httpServer = http.createServer(app).listen(process.env.PORT || 3001);
+// const httpsServer = https.createServer(httpsOptions, app);
+const server = app.listen(app.get("port"), () => {
+	console.log(
+		"  App is running at http://localhost:%d in %s mode",
+		app.get("port"),
+		app.get("env")
+	);
+	console.log("  Press CTRL-C to stop\n");
+});
+
+export default server;
