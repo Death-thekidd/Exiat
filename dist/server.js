ad42f911330d94d82f4b508b4b8ce79f1f1f7e58
deaththekidd <ohiemidivine7@gmail.com>
2023-09-10 04:20:14 +0100

    update

--- dist/server.js
+++ dist/server.js
@@ -20,9 +20,5 @@ if (process.env.NODE_ENV === "development") {
 // };
 // const httpServer = http.createServer(app).listen(process.env.PORT || 3001);
 // const httpsServer = https.createServer(httpsOptions, app);
-const server = app_1.default.listen(app_1.default.get("port"), () => {
-    console.log("  App is running at http://localhost:%d in %s mode", app_1.default.get("port"), app_1.default.get("env"));
-    console.log("  Press CTRL-C to stop\n");
-});
-exports.default = server;
+exports.default = app_1.default;
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -21,13 +21,5 @@ if (process.env.NODE_ENV === "development") {
 
 // const httpServer = http.createServer(app).listen(process.env.PORT || 3001);
 // const httpsServer = https.createServer(httpsOptions, app);
-const server = app.listen(app.get("port"), () => {
-	console.log(
-		"  App is running at http://localhost:%d in %s mode",
-		app.get("port"),
-		app.get("env")
-	);
-	console.log("  Press CTRL-C to stop\n");
-});
 
-export default server;
+export default app;
