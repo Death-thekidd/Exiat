70527addf1579ddf96ca9eee0cccf00ab1108db3
deaththekidd <ohiemidivine7@gmail.com>
2023-09-10 04:33:55 +0100

    update

--- dist/server.js
+++ dist/server.js
@@ -11,14 +11,5 @@ const app_1 = __importDefault(require("./app"));
 if (process.env.NODE_ENV === "development") {
     app_1.default.use(errorhandler_1.default());
 }
-/**
- * Start Express server.
- */
-// const httpsOptions: any = {
-// 	Key: fs.readFileSync("./src/certificates/ssl.key", "utf8"),
-// 	cert: fs.readFileSync("./src/certificates/certificate.crt", "utf8"),
-// };
-// const httpServer = http.createServer(app).listen(process.env.PORT || 3001);
-// const httpsServer = https.createServer(httpsOptions, app);
 exports.default = app_1.default;
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
 import errorHandler from "errorhandler";
 import app from "./app";
-import https from "https";
-import http from "http";
-import fs from "fs";
 
 /**
  * Error Handler. Provides full stack
@@ -10,16 +7,5 @@ import fs from "fs";
 if (process.env.NODE_ENV === "development") {
 	app.use(errorHandler());
 }
-
-/**
- * Start Express server.
- */
-// const httpsOptions: any = {
-// 	Key: fs.readFileSync("./src/certificates/ssl.key", "utf8"),
-// 	cert: fs.readFileSync("./src/certificates/certificate.crt", "utf8"),
-// };
-
-// const httpServer = http.createServer(app).listen(process.env.PORT || 3001);
-// const httpsServer = https.createServer(httpsOptions, app);
 
 export default app;
